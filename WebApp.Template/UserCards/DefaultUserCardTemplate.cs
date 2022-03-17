ef8f651e1abd4682de87f1e72e3a4fc1ced0eb4d
Hasan Hüseyin Alav <alavhasan72892@gmail.com>
2022-03-18 01:30:58 +0300

    TemplateDesign Pattern completed

---
+++ WebApp.Template/UserCards/DefaultUserCardTemplate.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApp.Template.UserCards
+{
+    public class DefaultUserCardTemplate : UserCardTemplate
+    {
+        protected override string SetFooter()
+        {
+            return string.Empty;
+        }
+
+        protected override string SetPicture()
+        {
+            return $"<img class='card-img-top' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzld4j0UVTqlGqgaR_Kxv4IXjpGt7yCL5fMJA4QwnKPDEglh9r3lpMXRc0SwRnk_5lrMQ&usqp=CAU'>";
+        }
+    }
+}
