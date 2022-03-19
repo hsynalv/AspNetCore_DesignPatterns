7eb1c478a60fd45e7999f60c6812d4a959b4bda7
Hasan Hüseyin Alav <hhsynalv@gmail.com>
2022-03-19 20:43:56 +0300

    Decorator Design Pattern Template Completed

---
+++ WebApp.Decorator/Startup.cs
@@ -0,0 +1,123 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using WebApp.Decorator.Models;
+using WebApp.Decorator.Repositories;
+using WebApp.Decorator.Repositories.Decorator;
+
+namespace BaseProject
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddMemoryCache();
+            services.AddHttpContextAccessor();
+
+
+            #region Compile time aþamasýnda Scrutor ile otomatik decorator eklemek
+            services.AddScoped<IProductRepository, ProductRepository>().Decorate<IProductRepository, ProductRepositoryCacheDecorator>().Decorate<IProductRepository, ProductRepositoryLoggingDecorator>();
+            #endregion
+
+            #region Compile time aþamasýnda el ile decorator eklemek
+            //services.AddScoped<IProductRepository>(sp =>
+            //{
+            //    var context = sp.GetRequiredService<AppIdentityDbContext>();
+            //    var memoryCache = sp.GetRequiredService<IMemoryCache>();
+            //    var productRepository = new ProductRepository(context);
+            //    var logService = sp.GetRequiredService<ILogger<ProductRepositoryLoggingDecorator>>();
+
+
+            //    var cacheDecorator = new ProductRepositoryCacheDecorator(productRepository, memoryCache);
+
+            //    var logDecorator = new ProductRepositoryLoggingDecorator(cacheDecorator, logService);
+
+            //    return logDecorator;
+            //});
+            #endregion
+
+            #region Runtime aþamasýnda decorator eklemek
+
+            services.AddScoped<IProductRepository>(sp =>
+            {
+                var httpContextAccessor = sp.GetRequiredService<IHttpContextAccessor>();
+
+                var context = sp.GetRequiredService<AppIdentityDbContext>();
+                var memoryCache = sp.GetRequiredService<IMemoryCache>();
+                var productRepository = new ProductRepository(context);
+                var logService = sp.GetRequiredService<ILogger<ProductRepositoryLoggingDecorator>>();
+
+                if (httpContextAccessor.HttpContext.User.Identity.Name == "user1")
+                {
+                    var cacheDecorator = new ProductRepositoryCacheDecorator(productRepository, memoryCache);
+                    return cacheDecorator;
+                }
+
+                var logDecorator = new ProductRepositoryLoggingDecorator(productRepository, logService);
+
+                return logDecorator;
+            });
+
+            #endregion
+
+
+
+
+            services.AddDbContext<AppIdentityDbContext>(opt =>
+            {
+                opt.UseSqlServer(Configuration.GetConnectionString("SqlServer"));
+            });
+
+            services.AddIdentity<AppUser, IdentityRole>(opt =>
+            {
+                opt.User.RequireUniqueEmail = true;
+            }).AddEntityFrameworkStores<AppIdentityDbContext>();
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
